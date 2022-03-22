c080214fa065271b82d0cf92e64b7b54fcb05b87
antonijas <antonija.samarzija@gmail.com>
2022-03-22 16:24:20 +0100

    change WalletInfo

---
+++ src/main/java/com/example/elasticsearch/domain/WalletInfo.java
@@ -0,0 +1,42 @@
+package com.example.elasticsearch.domain;
+
+import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "transactions_by_wallet", type = "walletinfo")
+public class WalletInfo {
+
+  @Id
+  private String id;
+
+  @Field(type = FieldType.Nested)
+  private List<TransactionForWallet> transactions;
+
+  protected WalletInfo() {
+  }
+
+  public WalletInfo(String id, List<TransactionForWallet> transactions) {
+    this.id = id;
+    this.transactions = transactions;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public List<TransactionForWallet> getTransactions() {
+    return transactions;
+  }
+
+  public void setTransactions(List<TransactionForWallet> transactions) {
+    this.transactions = transactions;
+  }
+
+}
