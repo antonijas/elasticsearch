74d51b992264ab8ad3fc82c5d852b2767d622ac1
antonijas <antonija.samarzija@gmail.com>
2022-03-25 14:44:05 +0100

    add bulk update

--- src/main/java/com/example/elasticsearch/domain/DocumentMigrator.java
+++ src/main/java/com/example/elasticsearch/domain/DocumentMigrator.java
@@ -6,7 +6,6 @@ import java.text.ParseException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import javax.annotation.PreDestroy;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -15,10 +14,15 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.get.MultiGetRequest;
+import org.elasticsearch.action.get.MultiGetRequest.Item;
+import org.elasticsearch.action.get.MultiGetResponse;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -42,6 +46,7 @@ import org.springframework.stereotype.Component;
 public class DocumentMigrator implements ApplicationRunner {
 
   private final String DOC_ID = "770899393414434816|BITGO|TBTC|DEPOSIT";
+  private final String DOC_ID2 = "774930573352767488|BITGO|TBTC|DEPOSIT";
 
   @Value("${wallet_index}")
   private String wallet_index;
@@ -78,12 +83,13 @@ public class DocumentMigrator implements ApplicationRunner {
   @Override
   public void run(ApplicationArguments args) throws Exception {
     saveWalletInfo();
-    Optional<WalletInfo> doc = transactionsByWalletRepo.findById(DOC_ID);
-    if (doc.isPresent()) {
-      logger.info("fetched wallet info. transactions {}", doc.get().getTransactions());
+    Iterable<WalletInfo> docs = transactionsByWalletRepo.findAll();
+    for (WalletInfo doc : docs) {
+      logger.info("fetched wallet info. transactions {}", doc.getTransactions());
+
     }
     createIndex();
-    copyDocumentToNewIndex(doc.get());
+    copyDocumentToNewIndex(docs);
     // readFromStaging();
     String jsonString = "{" +
         "\"partnerId\":\"123\"" +
@@ -91,38 +97,52 @@ public class DocumentMigrator implements ApplicationRunner {
     addNewFields(jsonString);
   }
 
-  private void copyDocumentToNewIndex(WalletInfo doc) throws IOException {
+  private void copyDocumentToNewIndex(Iterable<WalletInfo> docs) throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
-    String val = objectMapper.writeValueAsString(doc);
-    IndexRequest r = new IndexRequest(wallet_index);
-    r.type("_doc");
-    r.id(doc.getId());
-    r.source(val, XContentType.JSON);
-    IndexResponse resp = client.index(r, RequestOptions.DEFAULT);
-    logger.info("doc moved to new index {}", resp);
-    Optional<WalletInfoCopy> w = walletInfoCopyRepo.findById(DOC_ID);
-    if (w.isPresent()){
-      logger.info("reading from new index with spring data repo... {}", w.get());
+    // TODO: bulk?
+    for (WalletInfo doc : docs) {
+      String val = objectMapper.writeValueAsString(doc);
+      IndexRequest r = new IndexRequest(wallet_index);
+      r.type("_doc");
+      r.id(doc.getId());
+      r.source(val, XContentType.JSON);
+      client.index(r, RequestOptions.DEFAULT);
     }
-
+    logger.info("docs moved to new index ");
+    Iterable<WalletInfoCopy> w = walletInfoCopyRepo.findAllById(List.of(DOC_ID, DOC_ID2));
+    for (WalletInfoCopy doc_copy : w) {
+      logger.info("reading from new index with spring data repo... {}", doc_copy);
+    }
   }
 
   private void addNewFields(String json) throws IOException {
-    GetRequest req = new GetRequest(wallet_index,
-        "_doc",
-        DOC_ID);
-    GetResponse response = client.get(req, RequestOptions.DEFAULT);
-    logger.info("reading document from {}. Number of fields before update: {}", wallet_index, response.getSource().size());
+    MultiGetRequest multiGetRequest = new MultiGetRequest();
+    multiGetRequest.add(new Item(wallet_index, "_doc", DOC_ID));
+    multiGetRequest.add(new Item(wallet_index, "_doc", DOC_ID2));
+    client.mget(multiGetRequest, RequestOptions.DEFAULT);
+
+    // get only one....
+    //GetRequest req = new GetRequest(wallet_index,
+    //   "_doc",
+    //  DOC_ID);
+    //GetResponse response = client.get(req, RequestOptions.DEFAULT);
 
     UpdateRequest r = new UpdateRequest(wallet_index, "_doc", DOC_ID);
     r.doc(json, XContentType.JSON);
+    r.type("_doc");
+    UpdateRequest r2 = new UpdateRequest(wallet_index, "_doc", DOC_ID2);
+    r2.doc(json, XContentType.JSON);
+    r2.type("_doc");
 
-    UpdateResponse updateResponse = client.update(
-        r, RequestOptions.DEFAULT);
+    BulkRequest bulkRequest = new BulkRequest();
+    bulkRequest.add(r);
+    bulkRequest.add(r2);
+
+
+
+    BulkResponse updateResponse = client.bulk(
+        bulkRequest, RequestOptions.DEFAULT);
     logger.info("added new fields... {}", updateResponse);
-
-    logger.info("reading document from {}. Number of fields after update: {}", wallet_index,
-        client.get(req, RequestOptions.DEFAULT).getSource().size());
   }
 
 
@@ -158,7 +178,6 @@ public class DocumentMigrator implements ApplicationRunner {
         DOC_ID);
     GetResponse response = client.get(req, RequestOptions.DEFAULT);
     logger.info("reading from staging.... {}", response.getSource());
-
   }
 
   @PreDestroy
@@ -168,47 +187,52 @@ public class DocumentMigrator implements ApplicationRunner {
     DeleteIndexRequest deleteRequest = new DeleteIndexRequest(wallet_index);
     Boolean response = client.indices().delete(deleteRequest, RequestOptions.DEFAULT).isAcknowledged();
     logger.info("index deleted: {}", response);
-
   }
 
   private void saveWalletInfo() throws ParseException {
-    Map<String, Object> walletInfo1 = new HashMap<>();
-    walletInfo1.put("fees", "0.00000000");
-    walletInfo1.put("amount", "0.03198000");
-    walletInfo1.put("purchasePrice", "61533.58");
-    walletInfo1.put("type", "REDEEM");
-    walletInfo1.put("toAddress", "");
-    walletInfo1.put("internalId", "REDEEM|770900624442130432");
-    walletInfo1.put("redeemCode", "VYW2-62W4-U310");
-    walletInfo1.put("at", "2021-10-28T06:44:04.60693Z");
-    walletInfo1.put("redemptionTimestamp", "2021-10-28T06:44:02.975351Z");
-    walletInfo1.put("phoneNumber", "2069706916");
-    walletInfo1.put("creationTimestamp", "2021-10-28T06:43:51.854976Z");
-    walletInfo1.put("fromAddress", "");
-    walletInfo1.put("id", "REDEEM|770900624442130432");
-    walletInfo1.put("state", "CONFIRMED");
-    walletInfo1.put("quoteCurrencyAmount", "2050.00");
+    Map<String, Object> transactionInfo1 = new HashMap<>();
+    transactionInfo1.put("fees", "0.00000000");
+    transactionInfo1.put("amount", "0.03198000");
+    transactionInfo1.put("purchasePrice", "61533.58");
+    transactionInfo1.put("type", "REDEEM");
+    transactionInfo1.put("toAddress", "");
+    transactionInfo1.put("internalId", "REDEEM|770900624442130432");
+    transactionInfo1.put("redeemCode", "VYW2-62W4-U310");
+    transactionInfo1.put("at", "2021-10-28T06:44:04.60693Z");
+    transactionInfo1.put("redemptionTimestamp", "2021-10-28T06:44:02.975351Z");
+    transactionInfo1.put("phoneNumber", "2069706916");
+    transactionInfo1.put("creationTimestamp", "2021-10-28T06:43:51.854976Z");
+    transactionInfo1.put("fromAddress", "");
+    transactionInfo1.put("id", "REDEEM|770900624442130432");
+    transactionInfo1.put("state", "CONFIRMED");
+    transactionInfo1.put("quoteCurrencyAmount", "2050.00");
 
-    Map<String, Object> walletInfo2 = new HashMap<>();
-    walletInfo2.put("fees", "0.00000000");
-    walletInfo2.put("amount", "0.00040618");
-    walletInfo2.put("purchasePrice", "68495.14");
-    walletInfo2.put("type", "REDEEM");
-    walletInfo2.put("toAddress", "");
-    walletInfo2.put("internalId", "REDEEM|774963079862550528");
-    walletInfo2.put("redeemCode", "AN8R-G548-TW2U");
-    walletInfo2.put("at", "2021-11-08T11:46:41.660504Z");
-    walletInfo2.put("redemptionTimestamp", "2021-11-08T11:46:40.781795Z");
-    walletInfo2.put("phoneNumber", "2069706916");
-    walletInfo2.put("creationTimestamp", "2021-11-08T11:46:36.639565Z");
-    walletInfo2.put("fromAddress", "");
-    walletInfo2.put("id", "REDEEM|774963079862550528");
-    walletInfo2.put("state", "CONFIRMED");
-    walletInfo2.put("quoteCurrencyAmount", "29.00");
+    Map<String, Object> transactionInfo2 = new HashMap<>();
+    transactionInfo2.put("fees", "0.00000000");
+    transactionInfo2.put("amount", "0.00040618");
+    transactionInfo2.put("purchasePrice", "68495.14");
+    transactionInfo2.put("type", "REDEEM");
+    transactionInfo2.put("toAddress", "");
+    transactionInfo2.put("internalId", "REDEEM|774963079862550528");
+    transactionInfo2.put("redeemCode", "AN8R-G548-TW2U");
+    transactionInfo2.put("at", "2021-11-08T11:46:41.660504Z");
+    transactionInfo2.put("redemptionTimestamp", "2021-11-08T11:46:40.781795Z");
+    transactionInfo2.put("phoneNumber", "2069706916");
+    transactionInfo2.put("creationTimestamp", "2021-11-08T11:46:36.639565Z");
+    transactionInfo2.put("fromAddress", "");
+    transactionInfo2.put("id", "REDEEM|774963079862550528");
+    transactionInfo2.put("state", "CONFIRMED");
+    transactionInfo2.put("quoteCurrencyAmount", "29.00");
 
-    WalletInfo walletInfo = new WalletInfo(
+    WalletInfo walletInfo1 = new WalletInfo(
         DOC_ID,
-        List.of(new TransactionForWallet(walletInfo1), new TransactionForWallet(walletInfo2)));
-    transactionsByWalletRepo.save(walletInfo);
+        List.of(new TransactionForWallet(transactionInfo1), new TransactionForWallet(transactionInfo2)));
+
+    WalletInfo walletInfo2 = new WalletInfo(
+        DOC_ID2,
+        List.of(new TransactionForWallet(transactionInfo1)));
+    transactionsByWalletRepo.saveAll(List.of(walletInfo1, walletInfo2));
+
+
   }
 }
